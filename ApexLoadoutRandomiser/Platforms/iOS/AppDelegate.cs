b3340d13c67a263be73ee0544bf443e7934d6434
Paco <pacoli.contact@gmail.com>
2026-07-22 01:13:17 +0100

    initialised project

---
+++ ApexLoadoutRandomiser/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ApexLoadoutRandomiser;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
